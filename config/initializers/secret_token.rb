a0d65a7458893d60c28e34188e8b35e7bf264e99
Chau Tran <chau_tran@brown.edu>
2012-05-30 19:00:02 +0700

    Initialize project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studium::Application.config.secret_token = '3449e272a832d59933062062ae6f0c4b814aa5ea265a635826df94247f1c54462ee91446dde2c7ab088173faf148a167db8bf595f90ef4c5f8a7f3d036838899'

0e5b3a07c071859c1dfbf838066b652bc80ec5dc
Kien Hoang <kienhoangdev@gmail.com>
2012-06-02 23:55:55 +0700

    Fixed bug in link to profile.

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -3,3 +3,9 @@ Given /^the following users exist:$/ do |table|
     @user=User.create! user
   end
 end
+
+Given /^user "(.+)" has the following profile info:$/ do |email,table|
+  table.hashes.each do |profile_info|
+    @profile=Profile.create! profile_info.merge!(user_id: User.find_by_emaiL!(email).id)
+  end
+end

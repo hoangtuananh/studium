248544f883efd0bda460243004c4ddfd2e6615a4
Kien Hoang <kienhoangdev@gmail.com>
2012-06-10 23:13:58 +0700

    Testing Parser.

---
+++ script/test.rb
@@ -0,0 +1,16 @@
+class String
+	def highlight(question_id)
+		if question_id
+			result=self
+			while result=~/(.*)<hl #{question_id}>(.*?)<\/hl #{question_id}>(.*)/m
+				result=$1+"<hl>"+$2+"</hl>"+$3
+			end
+
+			result
+		end
+	end
+
+end
+
+s="<hl 89>asdsadsadsadsa<hl 90>Dsadsad</hl 90>dsadsa</hl 89>"
+puts (s.highlight(89).highlight(90))

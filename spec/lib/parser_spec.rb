8d9e22b675ff0969148e5b6f95c04f1c8d9c55e0
Kien Hoang <kienhoangdev@gmail.com>
2012-06-11 21:32:46 +0700

    Modified parser_spec a bit (before pulling).

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe String do
+describe Parser do
   it "should parse texts containing highlight markup tags" do
     test="<hl 90>Testing highlight markup</hl 90>"
     Parser.highlight(test,90).should eql("<hl 90>Testing highlight markup</hl 90>")

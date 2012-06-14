f6cbf8d9326ef4267253c271e167459b0ad862c5
Chau Tran <chau_tran@brown.edu>
2012-06-14 22:49:17 +0700

    Fix a minor bug. Used rake 0.9.2.2 (seriously wtf's going on with rake)

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to :question_type
   has_many :choices
-  accepts_nested_attributes_for :choices, :reject_if => lambda { |c| c.content.blank?}
+  accepts_nested_attributes_for :choices, :reject_if => lambda { |c| c[:content].blank?}
 end

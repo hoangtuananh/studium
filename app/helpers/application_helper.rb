6545199cf1c3d1535e5898fc45282b700625ae96
Kien Hoang <kienhoangdev@gmail.com>
2012-06-16 13:50:46 +0700

    Styled the paragraphs#new form a little bit. Haven't figured out how to fix add question.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,15 +3,15 @@ module ApplicationHelper
     content_for(:head) {javascript_include_tag(*files)}
   end
   def link_to_remove_question(name, f)
-    f.hidden_field(:_destroy) + link_to(name,"#",onclick: "remove_question(this)")
+    f.hidden_field(:_destroy) + link_to(name,"#",onclick: "remove_question(this)",class: "btn btn-danger")
   end
 
   def link_to_add_question(name, f, index, question_type_id, paragraph)
     new_question = paragraph.questions.build(question_type_id: question_type_id)
     new_question.choices.build(choice_letter: cycle("A","B","C","D","E"))
     fields = f.fields_for :questions do |builder|
-      render("questions_fields", :f => builder, index: index, question_type_id: question_type_id)
+       render("questions_fields",:f => builder, index: index, question_type_id: question_type_id)
     end
-    link_to(name,"#", onclick: "add_question(this, \"#{escape_javascript(fields)}\")")
+    link_to(name,"#", onclick: "add_question(this, \"#{escape_javascript(fields)}\")",class: "btn btn-primary")
   end
 end

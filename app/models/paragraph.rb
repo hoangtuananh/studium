19f0ae771284dc83b11f1456819a16f982376760
Kien Hoang <kienhoangdev@gmail.com>
2012-06-18 02:26:58 +0700

    Fixed some bug with toggling open the accordion when click edit.

--- app/models/paragraph.rb
+++ app/models/paragraph.rb
@@ -3,6 +3,7 @@ class Paragraph < ActiveRecord::Base
   accepts_nested_attributes_for :questions, :reject_if => lambda { |q| q[:title].blank?}, :allow_destroy => true
   validates :title, :content, presence: true
   validate :contains_questions
+
   def contains_questions
     if self.questions.empty?
       errors.add(:question, "Paragraph must have at least one question")

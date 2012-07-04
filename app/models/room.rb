51442a111ed5d0657d7a85f64ff2a6b2ea20dc8f
Chau Tran <chau_tran@brown.edu>
2012-07-04 19:35:23 +0700

    Deleted attr_acessible from Room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,4 @@
 class Room < ActiveRecord::Base
-  attr_accessible :current_question, :last_activity
   belongs_to :question
   has_many :questions_buffers
   has_many :questions, :through => :questions_buffers
